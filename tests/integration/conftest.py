f7019eec91bf98b2c0ac22eda854c7a4f238ba31
Eivind Jahren <ejah@equinor.com>
2025-10-15 10:13:03 +0200

    Make it possible to set simulator cmd through env variable

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,14 @@
 import pytest
 import shutil
+import os
 
 
 @pytest.fixture
 def simulator_cmd():
-    simulator_cmd = shutil.which("flow")
+    simulator_cmd = os.environ.get("TEST_SIMULATOR_CMD") or shutil.which("flow")
     if not simulator_cmd:
-        pytest.skip(reason="flow not available")
-    return [simulator_cmd]
+        pytest.skip(
+            reason="Did not find OPM flow simulator"
+            " and no simulator set in TEST_SIMULATOR_CMD"
+        )
+    return simulator_cmd.split()
