0c56e8ebd378be0f857f77ffdd17d68fa3b5c3eb
Eivind Jahren <ejah@equinor.com>
2025-11-03 14:53:18 +0100

    Add testing of summary reader error handling

--- tests/unit/test_summary_reader.py
+++ tests/unit/test_summary_reader.py
@@ -6,6 +6,8 @@ import pytest
 from hypothesis import given
 import hypothesis.strategies as st
 from contextlib import suppress
+import resfo
+import numpy as np
 
 
 def test_that_summary_reader_can_be_initialized_with_either_path_or_io(tmp_path: Path):
@@ -63,3 +65,89 @@ def test_that_the_read_values_matches_those_in_the_input(summary, report_step_on
     for kw_index, _ in enumerate(summary.summary_keywords):
         for report_step, val in enumerate(values):
             assert getter(unsmry, report_step, kw_index) == pytest.approx(val[kw_index])
+
+
+def read_summary(smspec, unsmry, report_step_only=True):
+    summary = SummaryReader(smspec=lambda: smspec, summaries=[lambda: unsmry])
+    _ = list(summary.values(report_step_only))
+    _ = list(summary.summary_keywords)
+
+
+@pytest.mark.parametrize(
+    "spec_contents, smry_contents, error_message",
+    [
+        (b"", b"", "Keyword startdat missing"),
+        (b"1", b"1", "Summary files contained invalid contents"),
+        (
+            b"\x00\x00\x00\x10FOOOOOOO\x00\x00\x00\x01"
+            + b"INTE"
+            + b"\x00\x00\x00\x10"
+            + (4).to_bytes(4, signed=True, byteorder="big")
+            + b"\x00" * 4,
+            b"",
+            "Keyword startdat missing",
+        ),
+        (
+            b"\x00\x00\x00\x10STARTDAT\x00\x00\x00\x01"
+            + b"INTE"
+            + b"\x00\x00\x00\x10"
+            + (4).to_bytes(4, signed=True, byteorder="big")
+            + b"\x00" * 4
+            + (4).to_bytes(4, signed=True, byteorder="big"),
+            b"",
+            "contains invalid STARTDAT",
+        ),
+    ],
+)
+def test_that_incorrect_summary_files_raises_informative_errors(
+    smry_contents, spec_contents, error_message
+):
+    smry_buf = BytesIO(smry_contents)
+    smry_buf.seek(0)
+    spec_buf = BytesIO(spec_contents)
+    spec_buf.seek(0)
+
+    with pytest.raises(InvalidSummaryError, match=error_message):
+        read_summary(spec_buf, smry_buf)
+
+
+@given(summaries())
+def test_truncated_summary_file_raises_invalidresponsefile(summary):
+    smspec, unsmry = summary
+    smspec_buf = BytesIO()
+    unsmry_buf = BytesIO()
+    smspec.to_file(smspec_buf)
+    unsmry.to_file(unsmry_buf)
+    smspec_buf.seek(0)
+    unsmry_buf.seek(0)
+    unsmry_buf.truncate(10)
+
+    with pytest.raises(InvalidSummaryError, match=""):
+        read_summary(smspec_buf, unsmry_buf)
+
+
+def test_mess_values_in_summary_files_raises_informative_errors():
+    smspec_buf = BytesIO()
+    resfo.write(smspec_buf, [("KEYWORDS", resfo.MESS)])
+    smspec_buf.seek(0)
+
+    with pytest.raises(InvalidSummaryError, match="has incorrect type MESS"):
+        read_summary(smspec_buf, BytesIO())
+
+
+def test_missing_keywords_in_smspec_raises_informative_error():
+    smspec_buf = BytesIO()
+    resfo.write(
+        smspec_buf,
+        [
+            ("STARTDAT", np.array([31, 12, 2012, 00], dtype=np.int32)),
+            ("UNITS   ", ["ANNUAL  "]),
+        ],
+    )
+    smspec_buf.seek(0)
+
+    with pytest.raises(
+        InvalidSummaryError,
+        match="Keywords missing",
+    ):
+        read_summary(smspec_buf, BytesIO())
