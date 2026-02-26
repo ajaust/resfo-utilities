deab1cb53038bf64397a0fa7d3c0d3cbeb0cf17c
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2026-02-26 09:58:28 +0100

    WIP: Some clenup

--- src/resfo_utilities/_cpp/interval_tree.cpp
+++ src/resfo_utilities/_cpp/interval_tree.cpp
@@ -104,73 +104,6 @@ std::vector<PillarBoundingBox> create_pillar_bounding_boxes(const float* coord, 
     std::vector<PillarBoundingBox> boxes;
     boxes.reserve(dims.ni * dims.nj);
 
-    //Eigen::Map<const Eigen::VectorXf, Eigen::Unaligned, Eigen::InnerStride<6>> xcoords(coord, dims.ni);
-    //Eigen::Map<const Eigen::VectorXf, Eigen::Unaligned, Eigen::InnerStride<6>> ycoords(coord + 1, dims.nj);
-//
-    //std::array<int, 4> pillar_indices = {0, 0, 0, 0};
-    //for (int i = 0; i < dims.ni; ++i) {
-    //    for (int j = 0; j < dims.nj; ++j) {
-    //        int pillar_idx[4] = {
-    //            (i * (dims.nj + 1) + j) ,
-    //            (i * (dims.nj + 1) + (j + 1)) ,
-    //            ((i + 1) * (dims.nj + 1) + j) ,
-    //            ((i + 1) * (dims.nj + 1) + (j + 1))
-    //        };
-    //        //pillar_indices[0] = i + j * dims.nj;
-    //        //pillar_indices[1] = pillar_indices[0] + 1;
-    //        //pillar_indices[2] = pillar_indices[0] + dims.nj;
-    //        //pillar_indices[3] = pillar_indices[2] + 1;
-    //        //pillar_indices[1] = i + j * dims.nj + 1;
-    //        //pillar_indices[2] = i + (j + 1) * dims.nj;
-    //        //pillar_indices[3] = i + (j + 1) * dims.nj + 1;
-
-    //        resfo::PillarBoundingBox box;
-    //        for (const auto& index: pillar_idx) {
-    //            box.min_x = std::min(box.min_x, xcoord[index]);
-    //            box.max_x = std::max(box.max_x, xcoord[index]);
-    //            box.min_y = std::min(box.min_y, ycoord[index+1]);
-    //            box.max_y = std::max(box.max_y, ycoord[index+1]);
-    //        }
-
-    //        boxes.push_back(std::move(box));
-    //    }
-    //}
-
-    //auto minmax_coord = [&coord](const int pillar_idx[4]) {
-    //    float min_val = std::numeric_limits<float>::max();
-    //    float max_val = std::numeric_limits<float>::lowest();
-
-    //    for (int v = 0; v < 4; ++v) {
-    //        const int& idx = pillar_idx[v];
-    //        min_val = std::min(min_val, coord[idx]);
-    //        max_val = std::max(max_val, coord[idx]);
-
-    //        min_val = std::min(min_val, coord[idx + 3]);
-    //        max_val = std::max(max_val, coord[idx + 3]);
-    //    }
-
-    //    return std::pair<float, float>{min_val, max_val};
-    //};
-
-    //auto xy_corners = [&xcoords, &ycoords](const int pillar_idx[4]) {
-    //    std::array<float, resfo::NUM_CORNERS > xcorners;
-    //    std::array<float, resfo::NUM_CORNERS > ycorners;
-    //    for (int v = 0; v < 4; ++v) {
-    //        int idx = pillar_idx[v];
-    //        xcorners[v] = xcoords[idx];
-    //        ycorners[v] = ycoords[idx];
-    //    }
-    //    for (int v = 0; v < 4; ++v) {
-    //        int idx = pillar_idx[v];
-    //        xcorners[v + 4] = xcoords[idx + 3];
-    //        ycorners[v + 4] = ycoords[idx + 3];
-    //    }
-    //    return std::pair<
-    //            std::array<float, resfo::NUM_CORNERS>,
-    //            std::array<float, resfo::NUM_CORNERS>
-    //        >{xcorners, ycorners};
-    //};
-
     for (int i = 0; i < dims.ni; ++i) {
         for (int j = 0; j < dims.nj; ++j) {
             const int pillar_idx[4] = {
@@ -180,41 +113,7 @@ std::vector<PillarBoundingBox> create_pillar_bounding_boxes(const float* coord, 
                 ((i + 1) * (dims.nj + 1) + (j + 1)) * 6
             };
 
-            //std::array<float, 3> top[4], bot[4];
-            //for (int p = 0; p < 4; ++p) {
-            //    top[p] = {coord[pillar_idx[p]], coord[pillar_idx[p] + 1], coord[pillar_idx[p] + 2]};
-            //    bot[p] = {coord[pillar_idx[p] + 3], coord[pillar_idx[p] + 4], coord[pillar_idx[p] + 5]};
-            //}
-
-            //for (int v = 0; v < resfo::NUM_CORNERS; ++v) {
-            //    int p_idx = pillar_idx[v];
-            //    xcoords[v] = coord[p_idx];
-            //    ycoords[v] = coord[p_idx + 1];
-            //    xcoords[v + 3] = coord[p_idx + 3];
-            //    xcoords[v + 3] = coord[p_idx + 3 + 1];
-            //}
-
-            //PillarBoundingBox box;
-            //{
-            //    auto [min_x_it, max_x_it] = std::minmax_element(xcoords.begin(), xcoords.end());
-            //    box.min_x = *min_x_it;
-            //    box.max_x = *max_x_it;
-            //    auto [min_y_it, max_y_it] = std::minmax_element(ycoords.begin(), ycoords.end());
-            //    box.min_y = *min_y_it;
-            //    box.max_y = *max_y_it;
-            //}
-
-
             PillarBoundingBox box;
-            //auto [xcorners, ycorners] = xy_corners(pillar_idx);
-            // for (int v = 0; v < resfo::NUM_CORNERS; ++v) {
-            //    const float& x = xcorners[v];
-            //    const float& y = ycorners[v];
-            //    box.min_x = std::min(box.min_x, x);
-            //    box.max_x = std::max(box.max_x, x);
-            //    box.min_y = std::min(box.min_y, y);
-            //    box.max_y = std::max(box.max_y, y);
-            //}
             for (int v = 0; v < 4; ++v) {
                 int idx = pillar_idx[v];
                 box.min_x = std::min(box.min_x, coord[idx]);
